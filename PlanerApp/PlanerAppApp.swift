7a52e85d82f04ce6a0f7ac11d815c145d8390cf5
Simone <simone.panico@icloud.com>
2023-08-24 19:38:09 +0200

    Initial Commit

---
+++ PlanerApp/PlanerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanerAppApp.swift
+//  PlanerApp
+//
+//  Created by Simone Panico on 24.08.23.
+//
+
+import SwiftUI
+
+@main
+struct PlanerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
